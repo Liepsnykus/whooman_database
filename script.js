9bd44ffbcf4a3c938352f69df4335ac8b575499d
Liepsnykus <74896017+Liepsnykus@users.noreply.github.com>
2021-02-28 23:07:09 +0200

    taking data from inputs. fetch update working

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const tBody = document.getElementById('tBody')
 
-function getItems() {
+let previousField = ''
+let previousFieldValue = ''
+let previousFieldParam = ''
 
-    fetch('http://localhost/php_rest_whoomans/api/dataList/read.php')
+
+const fetchFn = {
+    getItems: function() {
+        fetch('http://localhost/php_rest_whoomans/api/dataList/read.php')
         .then(resp => resp.json())
         .then(data => {
-            // console.log(data);
             createTable(data);
         })
         .catch(error => console.error(error));
+    },
+
+    updateRow: function(data) {
+        fetch('http://localhost/php_rest_whoomans/api/dataList/update.php', {
+            method: 'PUT',
+            body: JSON.stringify(data),
+        }) .then(resp => resp.json())
+        .then(data => {
+            
+            
+        })
+    }
 }
 
 function createTable(data) {
@@ -35,10 +51,10 @@ function createTable(data) {
         td5.classList.add('field')
         td5.innerText = item.gender
         let td6 = document.createElement('td')
-        td6.classList.add('field', 'deleteBtn', 'text-center')
+        td6.classList.add('deleteBtn', 'text-center')
         let deleteBtn = document.createElement('i')
         deleteBtn.classList.add('far', 'fa-trash-alt')
-       
+
 
         tBody.prepend(tRow)
         tRow.appendChild(td1)
@@ -49,62 +65,115 @@ function createTable(data) {
         tRow.appendChild(td6)
         td6.appendChild(deleteBtn)
 
-        td2.addEventListener('click', editField)
+        td2.addEventListener('click', openEditField)
         td2.myParam = 'nameField'
-        td3.addEventListener('click', editField)
+        td3.addEventListener('click', openEditField)
         td3.myParam = 'surnameField'
-        td4.addEventListener('click', editField)
+        td4.addEventListener('click', openEditField)
         td4.myParam = 'dateField'
-        td5.addEventListener('click', editField)
+        td5.addEventListener('click', openEditField)
         td5.myParam = 'genderField'
         td6.addEventListener('click', deleteRow)
     })
 
 }
 
-function editField(event) {
-    let editFieldParam = event.target.myParam
+function editField(data) {
+    let editFieldParam = data.target.myParam
 
     switch (editFieldParam) {
-        case 'nameField' :
-            editFunctions.editName(event)
+        case 'nameField':
+            editFunctions.editName(data)
+            fieldOpened = false
+            closeEditControls(data)
             break;
-        case 'surnameField' :
-            editFunctions.editSurname(event)
+        case 'surnameField':
+            editFunctions.editSurname(data)
+            fieldOpened = false
+            // closeEditControls(data)
             break;
-        case 'dateField' :
-            editFunctions.editDate(event)
+        case 'dateField':
+            editFunctions.editDate(data)
+            fieldOpened = false
+            // closeEditControls(data)
             break;
-        case 'genderField' :
-            editFunctions.editGender(event)
+        case 'genderField':
+            editFunctions.editGender(data)
+            fieldOpened = false
+            // closeEditControls(data)
             break;
     }
 
-    // if(editFieldParam == 'idField') {
-    //     editFunctions.editIdFn(event)
-    // }
-
-    // if(editFieldParam == 'nameField') {
-        
-    // }
-
+    console.log('sveiki');
 }
 
 const editFunctions = {
-    editName: function(data) {
+    editName: function (data) {
+
+        let rowData = {
+            id: data.path[3].children[0].innerText,
+            name: data.path[2].children[0].value,
+            surname: data.path[3].children[2].innerText,
+            date: data.path[3].children[3].innerText,
+            gender: data.path[3].children[4].innerText
+
+        }
+
+        fetchFn.updateRow(rowData)
+
         console.log('this came from name fn object');
-        console.log(data);
+        console.log(rowData);
     },
 
-    editSurname: function(data) {
+    editSurname: function (data) {
+
+        let rowData = {
+            id: data.path[3].children[0].innerText,
+            name: data.path[3].children[1].innerText,
+            surname: data.path[2].children[0].value,
+            date: data.path[3].children[3].innerText,
+            gender: data.path[3].children[4].innerText
+
+        }
+
+        fetchFn.updateRow(rowData)
         console.log('this came from edit Surname fn');
+        console.log(rowData);
     },
 
-    editDate: function(data) {
+    editDate: function (data) {
+        let rowData = {
+            id: data.path[3].children[0].innerText,
+            name: data.path[3].children[1].innerText,
+            surname: data.path[3].children[2].innerText,
+            date: data.path[2].children[0].value,
+            gender: data.path[3].children[4].innerText
+
+        }
+
+        fetchFn.updateRow(rowData)
+        console.log(rowData);
         console.log('this came from edit Date fn');
     },
 
-    editGender: function(data) {
+    editGender: function (data) {
+        console.log(data);
+        
+        let rowData = {
+            id: data.path[3].children[0].innerText,
+            name: data.path[3].children[1].innerText,
+            surname: data.path[3].children[2].innerText,
+            date: data.path[3].children[3].innerText,
+            gender: ''
+        }
+        if(data.path[2].children[0][0].checked) {
+            rowData.gender = 'Male'
+        } else {
+            rowData.gender = 'Female'
+        }
+
+        fetchFn.updateRow(rowData)
+        console.log(rowData);
         console.log('this came from editGender fn');
     }
 }
@@ -112,4 +181,108 @@ const editFunctions = {
 function deleteRow(event) {
     console.log(event);
 }
-getItems()
+
+function openEditField(event) {
+
+
+    closeEditControls(event)
+
+    let editFieldParam = event.target.myParam
+    let input
+    let value = event.target.innerText
+    let target = event.target
+    previousField = event.target.id
+    previousFieldValue = value
+    previousFieldParam = event.target.myParam
+    event.target.removeEventListener('click', openEditField)
+
+    target.innerHTML = ''
+    
+    
+    switch (editFieldParam) {
+        case 'nameField':
+            input = document.createElement('input')
+            input.classList.add('form-control')
+            input.setAttribute('type', 'text')
+            input.setAttribute('value', value)
+            break;
+
+        case 'surnameField':
+            input = document.createElement('input')
+            input.classList.add('form-control')
+            input.setAttribute('type', 'text')
+            input.setAttribute('value', value)
+            break;
+        case 'dateField':
+            input = document.createElement('input')
+            input.classList.add('form-control')
+            input.setAttribute('type', 'date')
+            input.setAttribute('value', value)
+            break;
+        case 'genderField':
+            input = document.createElement('form')
+            input.classList.add('form-check', 'form-check-inline')
+            let maleInput = document.createElement ('input')
+            maleInput.classList.add('form-check-input')
+            maleInput.setAttribute('type', 'radio')
+            maleInput.setAttribute('value', 'Male')
+            maleInput.setAttribute('name', 'gender')
+            maleInput.setAttribute('id', 'male')
+            value == 'Male' ? maleInput.setAttribute('checked', '') : ''
+            console.log(value);
+            let maleLabel = document.createElement('label')
+            maleLabel.setAttribute('for', 'male')
+            maleLabel.classList.add('form-check-label', 'mr-2')
+            maleLabel.innerText = 'Male'
+            let femaleInput = document.createElement ('input')
+            femaleInput.setAttribute('type', 'radio')
+            femaleInput.setAttribute('value', 'Female')
+            femaleInput.setAttribute('name', 'gender')
+            femaleInput.setAttribute('id', 'female')
+            femaleInput.classList.add('form-check-input')
+            value == 'Female' ? femaleInput.setAttribute('checked', '') : ''
+            let femaleLabel = document.createElement('label')
+            femaleLabel.setAttribute('for', 'female')
+            femaleLabel.classList.add('form-check-label')
+            femaleLabel.innerText = 'Female'
+            
+            input.appendChild(maleInput)
+            input.appendChild(maleLabel)
+            input.appendChild(femaleInput)
+            input.appendChild(femaleLabel)
+            break;
+    }
+
+
+    let div = document.createElement('div')
+    div.classList.add('editTools', 'd-flex')
+    saveBtn = document.createElement('button')
+    saveBtn.innerText = 'SAVE'
+    saveBtn.classList.add('btn', 'btn-primary', 'btn-sm',)
+    cancelBtn = document.createElement('button')
+    cancelBtn.innerText = 'CANCEL'
+    cancelBtn.classList.add('btn', 'btn-secondary', 'btn-sm',)
+
+    target.appendChild(input)
+    target.appendChild(div)
+    div.appendChild(saveBtn)
+    div.appendChild(cancelBtn)
+
+    saveBtn.addEventListener('click', editField)
+    saveBtn.myParam = event.target.myParam
+    cancelBtn.addEventListener('click', closeEditControls)
+    cancelBtn.myParam = event.target.myParam
+
+}
+
+function closeEditControls(event) {
+    let field = document.getElementById(previousField)
+    if (field) {
+        field.innerHTML = ''
+        field.innerText = previousFieldValue
+        field.myParam = previousFieldParam
+        field.addEventListener('click', openEditField)
+    }
+}
+
+fetchFn.getItems()
